f0a7a26eaa48b76ef06704be56f44ff27e0ccdc1
Gina <e.kurmeneva@gmail.com>
2020-07-19 15:57:06 +0300

    Add method to render all three rows

--- task3.js
+++ task3.js
@@ -8,12 +8,34 @@ const table = {
     },
     renderSubsequentRow: function ({name, date}) {
         return  this.tableBody.append("<tr><td>" + name + "</td><td>" + date + "</td></tr>");
+    },
+    renderTable: function (courses) {
+        this.renderFirstRow(courses[0]);
+        for (let i = 1; i < this.numberOfRows; i++) {
+            this.renderSubsequentRow(courses[i]);
+        }
+        return true;
     }
-    // renderTable: function (courses) {
-    //     this.renderFirstRow()
-    // }
 }
 
 table.renderFirstRow({name: "Событие 12", date: "25.08.2019"});
 table.renderSubsequentRow({name: "Событие 22", date: "25.08.2019"});
 table.renderSubsequentRow({name: "Событие 32", date: "25.08.2019"});
+
+table.renderTable([  {
+    name: 'Противодействие коррупции',
+    date: '28.02.2018'
+},
+    {
+        name: 'Обучение JS. Часть 1',
+        date: '27.02.2018'
+    },
+    {
+        name: 'Рекурсивные запросы MS SQL Server 2012',
+        date: '02.02.2019'
+    },
+    {
+        name: 'Управление IT-инфраструктурой (ITSM)',
+        date: '11.05.1992'
+    }
+]);
