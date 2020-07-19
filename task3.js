a0f4a5130f3505188e49063e7c6c6010c23ca150
Gina <e.kurmeneva@gmail.com>
2020-07-19 15:45:55 +0300

    Add method to render subsequent rows

--- task3.js
+++ task3.js
@@ -5,7 +5,12 @@ const table = {
     numberOfRows: 3,
     renderFirstRow: function (name, date) {
         return this.tableBody.html("<tr><td>" + name + "</td><td>" + date + "</td></tr>");
+    },
+    renderSubsequentRow: function (name, date) {
+        return  this.tableBody.append("<tr><td>" + name + "</td><td>" + date + "</td></tr>");
     }
 }
 
-table.renderFirstRow("Какое-то событие", "25.08.2019");
+table.renderFirstRow("Событие 1", "25.08.2019");
+table.renderSubsequentRow("Событие 2", "15.08.2020");
+table.renderSubsequentRow("Событие 3", "15.08.1990");
