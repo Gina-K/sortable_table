d7163fbc43487e461532be10df640e0d064434ca
Gina <e.kurmeneva@gmail.com>
2020-07-19 15:51:37 +0300

    Change arguments of the render methods to object

--- task3.js
+++ task3.js
@@ -3,14 +3,17 @@
 const table = {
     tableBody: $("#mainTableBody"),
     numberOfRows: 3,
-    renderFirstRow: function (name, date) {
+    renderFirstRow: function ({name, date}) {
         return this.tableBody.html("<tr><td>" + name + "</td><td>" + date + "</td></tr>");
     },
-    renderSubsequentRow: function (name, date) {
+    renderSubsequentRow: function ({name, date}) {
         return  this.tableBody.append("<tr><td>" + name + "</td><td>" + date + "</td></tr>");
     }
+    // renderTable: function (courses) {
+    //     this.renderFirstRow()
+    // }
 }
 
-table.renderFirstRow("Событие 1", "25.08.2019");
-table.renderSubsequentRow("Событие 2", "15.08.2020");
-table.renderSubsequentRow("Событие 3", "15.08.1990");
+table.renderFirstRow({name: "Событие 12", date: "25.08.2019"});
+table.renderSubsequentRow({name: "Событие 22", date: "25.08.2019"});
+table.renderSubsequentRow({name: "Событие 32", date: "25.08.2019"});
